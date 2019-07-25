2163a575635331235173e864a6945175028c368d
Luke Bertram <lukebertram@gmail.com>
2019-07-24 20:02:33 -0700

    set up React rendering at root path (via pages#index)

---
+++ app/javascript/packs/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+class App extends React.Component {
+	render() {
+		return (
+			<div>
+				Hello from React...
+				<h3>ON RAILS, DAWG!</h3>
+			</div>
+
+		)
+	}
+}
+
+export default App
